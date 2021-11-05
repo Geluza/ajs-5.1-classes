2c0635570a4760f6ebfaf80a022187585e023987
unknown <kgeluza@mail.ru>
2021-11-05 22:01:17 +0400

    commit4

--- src/js/__tests__/test.Bowerman.js
+++ src/js/__tests__/test.Bowerman.js
@@ -7,3 +7,19 @@ test('проверка персонажа Bowerman', () => {
   const received = new Bowerman('Alex', 'Bowman');
   expect(received).toEqual(expectedBowerman);
 });
+
+test('проверка конструктора на выброс исключения по типу данных name', () => {
+  expect(() => new Bowerman(123, 'Bowman')).toThrow('В поле name должны быть строчные значение, длина name должна быть больше 2 и не больше 10');
+});
+
+test('проверка конструктора на выброс исключения по длине name(min = 2)', () => {
+  expect(() => new Bowerman('A', 'Bowman')).toThrow('В поле name должны быть строчные значение, длина name должна быть больше 2 и не больше 10');
+});
+
+test('проверка конструктора на выброс исключения по длине name(max = 10)', () => {
+  expect(() => new Bowerman('Alexandrovich', 'Bowman')).toThrow('В поле name должны быть строчные значение, длина name должна быть больше 2 и не больше 10');
+});
+
+test('проверка конструктора на выброс исключения по типу персонажа', () => {
+  expect(() => new Bowerman('Alex', 'Vampir')).toThrow('Тип персонажа должен соответствовать одному из следующих: Bowman, Swordsman, Magician, Daemon, Undead, Zombie');
+});
